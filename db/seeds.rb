539e361854add950dff6225f7103738280409fdc
Larry Hawelu <lhawelu@gmail.com>
2021-04-27 14:38:47 -0700

    commit tree f6ee3cdffb4cbb45870b2d76d447d6535aa2f60b

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+
+Student.create(first_name: 'bob', last_name: 'bobby')
+Student.create(first_name: 'bob1', last_name: 'bobby1')
